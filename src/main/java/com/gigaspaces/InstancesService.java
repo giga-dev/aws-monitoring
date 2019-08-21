2033dd02cc190abc465b3475f56b60623d5f1399
Barak Bar Orion <barak.bar@gmail.com>
2019-08-21 12:17:32 +0300

    Spelling

--- src/main/java/com/gigaspaces/InstancesService.java
+++ src/main/java/com/gigaspaces/InstancesService.java
@@ -1,12 +1,18 @@
 package com.gigaspaces;
 
+import com.amazonaws.ClientConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.core.retry.backoff.FixedDelayBackoffStrategy;
+import software.amazon.awssdk.core.retry.conditions.MaxNumberOfRetriesCondition;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.*;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +31,15 @@ public class InstancesService {
                 try (Ec2Client client = Ec2Client.builder()
                         .region(region)
                         .credentialsProvider(ProfileCredentialsProvider.builder().profileName(profile).build())
+//                        .overrideConfiguration(ClientOverrideConfiguration.builder().retryPolicy())
+                        .overrideConfiguration(ClientOverrideConfiguration.builder()
+                                .apiCallAttemptTimeout(Duration.ofSeconds(30))
+                                .retryPolicy(RetryPolicy.builder()
+                                        .backoffStrategy(FixedDelayBackoffStrategy.create(Duration.ofSeconds(10)))
+                                        .numRetries(Integer.MAX_VALUE)
+                                        .retryCondition(MaxNumberOfRetriesCondition.create(Integer.MAX_VALUE))
+                                        .build())
+                                .build())
                         .build()) {
                     DescribeInstancesRequest runningReq = DescribeInstancesRequest.builder().filters(Filter.builder().name("instance-state-name").values("running").build()).build();
                     for (Reservation reservation : client.describeInstancesPaginator(runningReq).reservations()) {
